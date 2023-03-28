47d2e5a702ce623c111aaeeb68df1022b23cb3bd
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2023-03-28 13:07:35 +0300

    Removed parent imports in _app.jsz

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,5 @@
 import '@/styles/globals.css'
-import { Web3Provider } from '@ethersproject/providers'
-import { Web3ReactProvider } from '@web3-react/core'
 
-function getLibrary(provider) {
-  return new Web3Provider(provider)
-}
 export default function App({ Component, pageProps }) {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
-  )
+  return <Component {...pageProps} />
 }
